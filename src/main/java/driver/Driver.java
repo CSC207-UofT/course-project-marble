ca788e8c3686f2cb488c195bc0030bebe6b7566b
by20c <yangb32@gmail.com>
2021-10-10 17:31:52 -0400

    fixed naming

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -1,4 +1,35 @@
 package driver;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+
 public class Driver {
-}
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
+        if ("Hello".equals(greeting)) {
+            out.println("Hello! Welcome to our Bank!");
+        }
+    }
+        public void stop () throws IOException {
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        }
+        public static void main () throws IOException {
+            Driver server = new Driver();
+            server.start(80);
+        }
+    }
+
+
