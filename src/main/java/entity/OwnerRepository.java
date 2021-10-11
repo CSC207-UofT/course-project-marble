6cc0b72946c23300eee4557bb03cff9c1602734a
by20c <yangb32@gmail.com>
2021-10-11 19:31:33 -0400

    added Owner stuff

---
+++ src/main/java/entity/OwnerRepository.java
@@ -0,0 +1,20 @@
+package entity;
+
+import java.util.HashMap;
+
+public class OwnerRepository {
+    private HashMap<String, Owner> listOfOwners;
+
+    public OwnerRepository(){
+        // Read a txt file to initialize all the owners and their accounts
+    }
+
+    public boolean addOwner(){
+        return true;
+    }
+
+    public boolean loginOwner(String username, String password){
+
+        return true;
+    }
+}
