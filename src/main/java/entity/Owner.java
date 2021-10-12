bbd2f9345c549a3818f29b258d39b0b7a89140b8
by20c <yangb32@gmail.com>
2021-10-11 20:36:41 -0400

    added comparePassword, changes both under Owner, OwnerRepository

--- src/main/java/entity/Owner.java
+++ src/main/java/entity/Owner.java
@@ -1,17 +1,52 @@
 package entity;
 
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Owner {
-    private final String name;
-    private final String ID;
-    private final String password;
+    private final String fullName;
+    private final String username;
+    private final byte[] password;
     private HashMap<String, Account> Accounts;
 
-    public Owner(String name){
-        this.name = name;
-        ID = null;
-        password = null;
+
+    /**
+     * TODO: read text file implementation needs to be added.
+     */
+    public Owner(){
+        this.fullName = null;
+        this.password = null;
+        this.username = null;
     }
 
+    public boolean createAccount(String accountType){
+        return false;
+    }
+
+    /**
+     * This function comparePasswords, hashes the parameter inputPassword and
+     * compare it with the Owner's hashed password.
+     * @param inputPassword the password a user inputs is compared against the actual password, "hashed" comparison
+     *                      still needs to be implemented
+     * @return true if the password matches, else return false
+     */
+    public boolean comparePassword(String inputPassword){
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        assert md != null;
+        byte[] inputPasswordHashed = md.digest(inputPassword.getBytes(StandardCharsets.UTF_8));
+
+        if (this.password != null) {
+            return Arrays.equals(this.password, inputPasswordHashed);
+        }
+        return false;
+    }
 }
--- src/main/java/entity/OwnerRepository.java
+++ src/main/java/entity/OwnerRepository.java
@@ -1,20 +1,40 @@
 package entity;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 public class OwnerRepository {
+    /**
+     *
+     */
     private HashMap<String, Owner> listOfOwners;
 
+    /**
+     *
+     */
     public OwnerRepository(){
         // Read a txt file to initialize all the owners and their accounts
     }
+
 
     public boolean addOwner(){
         return true;
     }
 
-    public boolean loginOwner(String username, String password){
-
-        return true;
+    /**
+     * loginCheck is a method that Driver calls to check if the username and the password matches with the data
+     * stored here on OwnerRepository. It'll return True if the credential matches, otherwise false (e.g. wrong
+     * password or user does not exist).
+     * @param username checks with a list of Owner objects and see if the username matches with any of them.
+     * @param password if the username matches, check if the password matches as well.
+     * @return true if the password matches, return false if not
+     */
+    public boolean loginCheck(String username, String password) throws NoSuchAlgorithmException {
+        for(String key : listOfOwners.keySet()){
+            if(username.equals(key)){
+                return listOfOwners.get(key).comparePassword(password);
+            }
+        }
+        return false;
     }
 }
