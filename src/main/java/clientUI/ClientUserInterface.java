2cfe4666f366b33cd79cc154b7c292312c4df317
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2021-10-12 00:49:57 -0400

    Wrote Pseudocode for main method in ClientUserInterface

--- src/main/java/clientUI/ClientUserInterface.java
+++ src/main/java/clientUI/ClientUserInterface.java
@@ -8,12 +8,14 @@ public class ClientUserInterface {
     private Socket clientSocket;
     private ObjectOutputStream outbound;
     private ObjectInputStream inbound;
+    private String ownerID = null;
 
     public void Connect(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
-        outbound = new ObjectOutputStream(clientSocket.getOutputStream();
+        outbound = new ObjectOutputStream(clientSocket.getOutputStream());
         inbound = new ObjectInputStream(clientSocket.getInputStream());
     }
+
     public boolean login(String username, String password) throws IOException, ClassNotFoundException {
         String message =  (String) inbound.readObject();
         System.out.println(message);
@@ -29,5 +31,50 @@ public class ClientUserInterface {
         inbound.close();
         outbound.close();
         clientSocket.close();
+    }
+
+    public static void main(String[] args) {
+        // create a new client_user_interface called client
+        // connect the client_user_interface to server (ip, port)
+
+        System.out.println("Welcome to our Bank!");
+        System.out.println("Please type 'Login' to login to your account");
+        System.out.println("Type 'Quit' to exit the program");
+
+        // String userInput = ""
+        //
+        // while userInput != "quit" {
+        //      userInput = System input
+        //
+        //      while ownerID is null {
+        //
+        //          while userInput != "Login" {
+        //              System.out.println("Please type 'Login' to login to your account");
+        //              System.out.println("Type 'Quit' to exit the program");
+        //              userInput = System input
+        //          }
+        //
+        //          result = client.login(); // send LoginRequest to Server, get response, set ownerID to input ownerID
+        //
+        //          if result == True:
+        //          System.out.println("Please select an action in 'Deposit', 'Transfer',
+        //          'Withdrawal', 'View Account', 'CheckBalance', 'Get Transacton History'");
+        //
+        //          else:
+        //          System.out.println("Login information is not right")
+        //          System.out.println("Please type 'Login' to login to your account");
+        //          System.out.println("Type 'Quit' to exit the program");
+        //
+        //      }
+        //
+        //      if userInput == "Transfer" {
+        //          client.transfer();
+        //      } else if ... {
+        //          ...
+        //      } else {
+        //          System.out.println("Action not valid, please input another action.")
+        //      }
+        //
+        //}
     }
 }
