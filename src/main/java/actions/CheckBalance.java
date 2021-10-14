39f8acd62c8a03fc765f48318c93ade072a8c226
leya-595 <82251651+leya-595@users.noreply.github.com>
2021-10-14 17:46:27 -0400

    Updated CheckBalance.

--- src/main/java/actions/CheckBalance.java
+++ src/main/java/actions/CheckBalance.java
@@ -7,10 +7,15 @@ public class CheckBalance extends Actions{
     private Integer accountID;
     private final Owner owner;
 
+    public CheckBalance(Owner owner){
+        this.owner = owner;
+    }
+
     public CheckBalance(Owner owner, Integer accountID){
         this.accountID = accountID;
         this.owner = owner;
     }
+
     /**
      * This method checks the owners total account balance of a specific account.
      * @return return the balance of the owners account.
