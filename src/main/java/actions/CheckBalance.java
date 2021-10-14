24fe3b39e4391f2bcedf07217daad5e616e3f014
leya-595 <82251651+leya-595@users.noreply.github.com>
2021-10-13 22:05:33 -0400

    Updated CheckBalance and ViewAccounts.

--- src/main/java/actions/CheckBalance.java
+++ src/main/java/actions/CheckBalance.java
@@ -1,4 +1,33 @@
 package actions;
 
-public class CheckBalance {
+import entity.Account;
+import entity.Owner;
+
+public class CheckBalance extends Actions{
+    private Integer accountID;
+    private final Owner owner;
+
+    public CheckBalance(Owner owner, Integer accountID){
+        this.accountID = accountID;
+        this.owner = owner;
+    }
+    /**
+     * This method checks the owners total account balance of a specific account.
+     * @return return the balance of the owners account.
+     */
+    public int checkAccountBalance(){
+        return this.owner.getAccounts().get(this.accountID).getBalance();
+    }
+
+    /**
+     * This method checks the owners total account balance.
+     * @return return the total balance of the owners accounts.
+     */
+    public int checkTotalBalance() {
+        int total = 0;
+        for (Account account : this.owner.getAccounts().values()) {
+            total += account.getBalance();
+        }
+        return total;
+    }
 }
--- src/main/java/actions/ViewAccounts.java
+++ src/main/java/actions/ViewAccounts.java
@@ -1,4 +1,27 @@
 package actions;
 
-public class ViewAccounts {
+import entity.Account;
+import entity.Owner;
+
+import java.util.HashMap;
+
+public class ViewAccounts extends Actions {
+    private final Owner owner;
+
+    public ViewAccounts(Owner owner) {
+        this.owner = owner;
+    }
+
+    public Owner getOwner() {
+        return this.owner;
+    }
+
+    /**
+     * This method returns the owners accounts.
+     *
+     * @return return all the owners accounts.
+     */
+    public HashMap<Integer, Account> displayAccountsInfo() {
+        return this.owner.getAccounts();
+    }
 }
