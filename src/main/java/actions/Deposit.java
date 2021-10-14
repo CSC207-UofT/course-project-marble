09f1023afbdf7ce7be15455c10650c2174c198f6
anita-hp <anita.hessami@mail.utoronto.ca>
2021-10-14 15:17:11 -0400

    Updated Deposit

--- src/main/java/actions/Deposit.java
+++ src/main/java/actions/Deposit.java
@@ -1,4 +1,21 @@
 package actions;
+import entity.*;
+import java.util.HashMap;
 
-public class Deposit {
+public class Deposit extends Actions{
+    public Owner own;
+    public Deposit(Owner owner){
+        own = owner;
+    }
+    public String depositIn(int num, String id){
+        HashMap<Integer, Account> accounts = own.getAccounts();
+        if (accounts.get(Integer.valueOf(id)) == null){
+            return "Failed to deposit. Account with ID " +  id +
+                    " does not exist for this user.";
+        } else{
+            accounts.get(Integer.valueOf(id)).increaseBalance(num);
+            return "Successful deposit of $" + num +
+                    " to the account with ID" + id;
+        }
+    }
 }
