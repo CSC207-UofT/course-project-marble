f9e73510cc4f4d76a353f9967803002f658d4560
Aliyan Gohar <a.gohar@mail.utoronto.ca>
2021-11-04 10:24:48 -0400

    Preliminary finish of credit card

--- src/main/java/entity/CreditCard.java
+++ src/main/java/entity/CreditCard.java
@@ -2,21 +2,27 @@ package entity;
 
 public class CreditCard extends Depositable {
     public CreditCard() {
-        super(-19.9);
+        super(19.9);
     }
 
     @Override
     public double deposit(double amount) {
-        return 0;
+        double temp = this.getValue();
+        temp = temp - amount;
+        super.setBalance(temp);
+        return super.getBalance();
     }
 
     @Override
     public void addInterest() {
-
+        double monthlyInterest = super.getAnnualInterestRate() / 12;
+        double temp = super.getBalance();
+        temp = temp - (temp * (monthlyInterest / 100));
+        super.setBalance(temp);
     }
 
     @Override
     public double getValue() {
-        return 0;
+        return super.getBalance();
     }
 }
