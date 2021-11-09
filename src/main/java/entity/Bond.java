656370529d003eb434cea97fe3a58d8b4468b9ca
Aliyan Gohar <a.gohar@mail.utoronto.ca>
2021-11-09 16:07:59 -0500

    Seemingly finish up bond

--- src/main/java/entity/Bond.java
+++ src/main/java/entity/Bond.java
@@ -1,34 +1,49 @@
 package entity;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-public class Bond extends FinancialAsset {
-    private final SimpleDateFormat dateOfMaturity;
-    private final double pricePerBond;
-    private final int volume;
-    public Bond(Date dateCreated, int volume, float annualInterestRate, double pricePerBond, SimpleDateFormat dateOfMaturity) {
-        super(annualInterestRate);
+public class Bond extends NonDepositable {
+    private final Date dateOfMaturity;
+    private boolean cashedOut;
+    public Bond(int volume, float annualInterestRate, double pricePerBond, Date dateOfMaturity) {
+        super(annualInterestRate,  volume, pricePerBond);
+        cashedOut = false;
         this.dateOfMaturity = dateOfMaturity;
-        this.pricePerBond = pricePerBond;
-        this.volume = volume;
     }
 
     @Override
     public double getValue() {
-//        if (new SimpleDateFormat("MM/yyyy").parse(date).before(new Date())) {
-
-//        }
-        return 1;
+        if (cashedOut){
+            return 0;
+        }
+        Date dateToday = new Date();
+        if (dateToday.compareTo(this.dateOfMaturity) < 0) {
+            return 0;
+        }
+        return this.getPricePerBond() * super.getVolume();
     }
 
-    public SimpleDateFormat getDateOfMaturity() {
+    public Date getDateOfMaturity() {
         return this.dateOfMaturity;
     }
     public double getTotalCost(){
-        return this.pricePerBond * this.volume;}
+        return super.getPricePerAsset() * super.getVolume();}
 
     public double getPricePerBond() {
-        return this.pricePerBond;
+        return super.getPricePerAsset();
+    }
+
+    @Override
+    public double cashOut() {
+        if (cashedOut){
+            return -1;
+        }
+        Date dateToday = new Date();
+        if (dateToday.compareTo(this.dateOfMaturity) < 0) {
+            return -1;
+        }
+        double returnVal = this.getValue();
+        this.cashedOut = true;
+        return returnVal;
     }
 }
+
