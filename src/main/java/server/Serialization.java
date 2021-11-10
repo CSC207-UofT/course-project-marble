1140b986eedc172141f3f5cbfcf927f0934de3a1
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2021-11-10 05:04:38 -0500

    Serialization Works, we need to add implements serialization to classes that will be serialized, I did em on my local branch but will hold off on doing to main branch stuff till I look into serializing more to know if there are adverse things to look out for.

---
+++ src/main/java/server/Serialization.java
@@ -0,0 +1,70 @@
+package server;
+
+import entity.Bond;
+import entity.Date;
+
+import java.io.*;
+
+public class Serialization{
+    private FileOutputStream file;
+    private ObjectOutputStream out;
+    private FileInputStream fileIn;
+    private ObjectInputStream in;
+
+    public Serialization() throws IOException{
+        try{
+            file = new FileOutputStream("File_Data_Storing.TXT");
+            out = new ObjectOutputStream(file);
+            fileIn = new FileInputStream("File_Data_Storing.TXT");
+            in = new ObjectInputStream(fileIn);
+        }catch (IOException ex){
+            System.out.print("Serialization Class Initialization Error");
+        }
+    }
+
+    public void Serialize(Object obj) throws IOException {
+        try {
+            out.writeObject(obj);
+
+            out.close();
+            file.close();
+
+            System.out.println("Object has been serialized\n" + "Data before Deserialization.");
+            System.out.println(obj);//for testing purposes
+
+        }catch (IOException ex){
+            System.out.print("Serialization Error");
+        }
+    }
+
+    public Object Deserialize() throws IOException {
+        Object obj;
+        try{
+            obj = in.readObject();
+            return obj;
+        } catch (IOException i) {
+            i.printStackTrace();
+            System.out.print("Deserialization Error");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    //TESTING PURPOSES AND SHOWING HOW TO USE CLASS
+    public static void main(String []args) throws IOException {
+        Bond b = new Bond(3, 4, 3.23, new Date(2021,2,1));
+        printData(b);
+        Serialization ser = new Serialization();
+        ser.Serialize(b);
+        Bond bond2 = (Bond) ser.Deserialize();
+        printData(bond2);
+    }
+    public static void printData(Bond b){
+        System.out.println("Volume = " + b.getVolume());
+        System.out.println("Annual Int = " + b.getAnnualInterestRate());
+        System.out.println("Date of Maturity = " + b.getDateOfMaturity());
+        System.out.println("Price Per Bond = " + b.getPricePerBond());
+    }
+}
