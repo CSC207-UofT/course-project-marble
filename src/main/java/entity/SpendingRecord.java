762b5d4711a0c090ce488497cd72a1da697a66a0
leya-595 <82251651+leya-595@users.noreply.github.com>
2021-11-11 19:39:57 -0500

    Update SpendingRecord to use Date entity

--- src/main/java/entity/SpendingRecord.java
+++ src/main/java/entity/SpendingRecord.java
@@ -1,13 +1,11 @@
 package entity;
 
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class SpendingRecord{
 
     private double amount;
-    private LocalDateTime purchaseDate;
+    private Date purchaseDate;
     private ArrayList<SpendingRecord> spendingRecordList;
 
     /**
@@ -18,7 +16,7 @@ public class SpendingRecord{
      * @param purchaseDate : the date of purchase.
      */
 
-    public SpendingRecord(double amount, LocalDateTime purchaseDate){
+    public SpendingRecord(double amount, Date purchaseDate){
         this.amount = -amount;
         this.purchaseDate = purchaseDate;
     }
