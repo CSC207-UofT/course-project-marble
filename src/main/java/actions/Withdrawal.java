ad6366f339c3be92460919813677afac48871108
leya-595 <82251651+leya-595@users.noreply.github.com>
2021-11-11 19:33:34 -0500

    Updated Withdrawal to create a spending record.

--- src/main/java/actions/Withdrawal.java
+++ src/main/java/actions/Withdrawal.java
@@ -3,9 +3,11 @@ import entity.*;
 
 public class Withdrawal extends Actions{
     private Owner user;
+    private final Date date;
 
     public Withdrawal(Owner user){
         this.user = user;
+        this.date = new Date();
     }
 
     /**
@@ -15,6 +17,8 @@ public class Withdrawal extends Actions{
      */
     public boolean withdraw(int num){
         if (num <= user.getBalance()) {
+            SpendingRecord spending = new SpendingRecord(num, this.date);
+            user.addSpendingRecord(spending);
             user.setBalance(user.getBalance() - num);
             return true;
         }
