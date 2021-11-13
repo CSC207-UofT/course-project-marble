20acf799e6043767d95249b9a00c3322aabf80d6
anita-hp <anita.hessami@mail.utoronto.ca>
2021-11-13 12:47:33 -0500

    Updated UpdateDepositable and ViewInvestments

--- src/main/java/actions/UpdateDepositable.java
+++ src/main/java/actions/UpdateDepositable.java
@@ -2,5 +2,25 @@ package actions;
 
 import entity.Depositable;
 
+import entity.Date;
+
 public class UpdateDepositable{
+    private Depositable account;
+
+    public UpdateDepositable(Depositable account) {
+        this.account = account;
+    }
+    /**
+     * This method calculates the current balance of Depositable accounts.
+     * @return balance of Depositable account.
+     */
+    public double valueDepositable(){
+        Date d = new Date();
+        int numMonth = d.monthDifference(account.getDateCreated());
+        for(int i=0; i<numMonth;i++){
+            account.addInterest();
+        }
+        return account.getBalance();
+    }
+
 }
