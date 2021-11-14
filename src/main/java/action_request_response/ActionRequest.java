6e80bc5ab184f3e161b7f35cbb8416c66cb95dec
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2021-11-14 00:15:11 -0500

    Add documents for action request add loginResponseTest

--- src/main/java/action_request_response/ActionRequest.java
+++ src/main/java/action_request_response/ActionRequest.java
@@ -5,14 +5,29 @@ import java.io.Serializable;
 public abstract class ActionRequest implements Serializable {
     private final String username;
 
+    /**
+     * Constructor that creates an instance of ActionRequest given a username
+     *
+     * @param username String username of the user
+     */
     public ActionRequest(String username) {
         this.username = username;
     }
 
+    /**
+     * Returns whether the ActionRequest is a UserQuitRequest
+     *
+     * @return a boolean of whether the ActionRequest is a UserQuitRequest
+     */
     public boolean isQuit() {
         return false;
     }
 
+    /**
+     * Getter method for username in ActionRequest
+     *
+     * @return username stored in this ActionRequest
+     */
     public String getUsername() {
         return username;
     }
