508c0026bbe3cf4c37b4435d43b5aed42bcdcb2c
Linh <linhquan.hoang@mail.utoronto.ca>
2021-11-13 23:26:10 -0500

    Removed an import

--- src/test/java/actions/testAddExpense.java
+++ src/test/java/actions/testAddExpense.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
