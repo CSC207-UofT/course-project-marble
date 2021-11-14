bec583bdff8763e882f0c3bb4964bf8f7d790373
Linh <linhquan.hoang@mail.utoronto.ca>
2021-11-14 14:52:14 -0500

    Add tests for CompareBudget

--- src/main/java/actions/CompareBudget.java
+++ src/main/java/actions/CompareBudget.java
@@ -30,19 +30,24 @@ public class CompareBudget extends Actions{
 
     private String format(){
         ArrayList<String> categories = budget.getCategories();
-        String display = "Total Amount to Budget: " + Double.toString(total) + "\n";
-        display = display + "Budget Based on 50/30/20 Rule: \n" +
-                "Needs: $" + ruleBudget.get(0) + "\n" +
-                "Wants: $" + ruleBudget.get(1) + "\n" +
-                "Investment/Debt: $" + ruleBudget.get(2) + "\n" +
-                "Remaining amount: $0.00 \n --------------- \n";
-        display = display + "Current Budget (from Categories written in):" + "\n";
-        for (int i = 0; i < categories.size(); i++){
-            String categoryName = categories.get(i);
-            display = display + categoryName + ": $" +
-                    budget.getGoalCategoryBudget(categoryName) + "\n";
+        StringBuilder display = new StringBuilder();
+        display.append("Total Amount to Budget: $");
+        display.append(total);
+        display.append("\nBudget Based on 50/30/20 Rule: \nNeeds: $");
+        display.append(ruleBudget.get(0));
+        display.append("\nWants: $");
+        display.append(ruleBudget.get(1));
+        display.append("\nInvestment/Debt: $");
+        display.append(ruleBudget.get(2));
+        display.append("\nRemaining amount: $0.00 \n ---------------\n");
+        display.append("Current Budget (from Categories written in):\n");
+        for (String categoryName : categories) {
+            display.append(categoryName);
+            display.append(": $");
+            display.append(budget.getGoalCategoryBudget(categoryName));
+            display.append("\n");
         }
-        return display;
+        return display.toString();
     }
 
 
