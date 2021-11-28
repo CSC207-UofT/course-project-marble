e8272ae171305bfc0eb0eda3ebaf3c764590bb23
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2021-11-28 18:24:34 -0500

    Refactor login() method in ClientUserInterface and its related classes

--- src/main/java/clientUI/ClientUserInterface.java
+++ src/main/java/clientUI/ClientUserInterface.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientUserInterface {
