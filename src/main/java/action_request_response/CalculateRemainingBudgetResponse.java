64bfe22b7019e363b539ab54b378f9775bac67a4
Linh <linhquan.hoang@mail.utoronto.ca>
2021-12-01 02:46:07 -0500

    Added CalculateRemainingBudgetResponse class and completed class

---
+++ src/main/java/action_request_response/CalculateRemainingBudgetResponse.java
@@ -0,0 +1,23 @@
+package action_request_response;
+
+public class CalculateRemainingBudgetResponse extends ActionResponse{
+    private final boolean exists;
+    private final double amount;
+
+
+    public CalculateRemainingBudgetResponse(boolean exists) {
+        this.exists = exists;
+        this.amount = 0;
+    }
+
+    public  CalculateRemainingBudgetResponse(boolean exists, double amount) {
+        this.exists = exists;
+        this.amount = amount;
+    }
+    public String getResult(){
+        if (!exists) {
+            return "Budget Inactive or Does Not Exist";
+        }
+        return ("Remaining Budget: 4" + String.valueOf(amount));
+    }
+}
