d7040031447fac867f8dae58aea05f47c36da053
Linh <linhquan.hoang@mail.utoronto.ca>
2021-12-02 21:58:14 -0500

    added Disconnect

--- src/main/java/clientUI/CreateUser.java
+++ src/main/java/clientUI/CreateUser.java
@@ -8,16 +8,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CreateUser {
-    public ActionRequest createUser(){
+    public ActionRequest CreateUser(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter your full name");
+        System.out.println("Please enter your fullname");
         String fullName = sc.nextLine();
         System.out.println("Please enter your chosen username");
         String username = sc.nextLine();
         System.out.println("Please enter your password");
         String password = sc.nextLine();
 
-        return new ActionRequest(username, Commands.CREATEUSER,
+        ActionRequest request = new ActionRequest(username, Commands.CREATEUSER,
                 new ArrayList<>(List.of(fullName, password)));
+        return request;
     }
 }
