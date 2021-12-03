c4d0e70a1c6c6f568b20c3e75302f47aebcbe5b6
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2021-12-03 12:59:21 -0500

    -QuickPopup action for panel with list of options that lead to popup for input or display

--- src/main/java/GUI/StartFromBeginning/Done/QuickPopupActionPanel.java
+++ src/main/java/GUI/StartFromBeginning/Done/QuickPopupActionPanel.java
@@ -7,13 +7,18 @@ import java.awt.event.MouseEvent;
 
 public class QuickPopupActionPanel extends JPanel {
 
+    private JLabel createBond;
+    private JLabel createSavings;
+    private JLabel budgetGoal;
+
+
     public QuickPopupActionPanel(){
         super(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.insets = new Insets(10,10,10,10);
         constraints.fill = GridBagConstraints.HORIZONTAL;
 
-        JLabel budgetGoal=new JLabel("Budget Goal Progress");
+        budgetGoal = new JLabel("Budget Goal Progress");
         constraints.gridx = 0;
         constraints.gridy = 0;
 
@@ -21,33 +26,57 @@ public class QuickPopupActionPanel extends JPanel {
         budgetGoal.addMouseListener(new MouseAdapter(){
             public void mouseClicked(MouseEvent me)
             {
-                //show budget progress panel
+                BudgetGoalPanel goalPanel = new BudgetGoalPanel();
+                PopUp pop = new PopUp("Budget Goal Progress", goalPanel);
+                pop.getFrame().setVisible(true);
+            }
+            public void mouseEntered(MouseEvent me){
+                budgetGoal.setForeground(Color.BLUE);
+            }
+            public void mouseExited(MouseEvent me){
+                budgetGoal.setForeground(Color.BLACK);
             }
         });
 
         this.add(budgetGoal, constraints);
 
-        JLabel createBond = new JLabel("Create Bond");
+        createBond = new JLabel("Create Bond");
         constraints.gridy = 1;
 
         createBond.setCursor(new Cursor(Cursor.HAND_CURSOR));
         createBond.addMouseListener(new MouseAdapter(){
             public void mouseClicked(MouseEvent me)
             {
-                //show create bond popup
+                CreateBondPanel createBondPanel = new CreateBondPanel();
+                PopUp pop = new PopUp("Create Bond", createBondPanel);
+                pop.getFrame().setVisible(true);
+            }
+            public void mouseEntered(MouseEvent me){
+                createBond.setForeground(Color.BLUE);
+            }
+            public void mouseExited(MouseEvent me){
+                createBond.setForeground(Color.BLACK);
             }
         });
 
         this.add(createBond, constraints);
 
-        JLabel createSavings = new JLabel("Create Savings Account");
+        createSavings = new JLabel("Create Savings Account");
         constraints.gridy = 2;
 
         createSavings.setCursor(new Cursor(Cursor.HAND_CURSOR));
         createSavings.addMouseListener(new MouseAdapter(){
             public void mouseClicked(MouseEvent me)
             {
-                //show create savings popup
+                CreateSavingsPanel createSavingsPanel = new CreateSavingsPanel();
+                PopUp pop = new PopUp("Create Bond", createSavingsPanel);
+                pop.getFrame().setVisible(true);
+            }
+            public void mouseEntered(MouseEvent me){
+                createSavings.setForeground(Color.BLUE);
+            }
+            public void mouseExited(MouseEvent me){
+                createSavings.setForeground(Color.BLACK);
             }
         });
 
