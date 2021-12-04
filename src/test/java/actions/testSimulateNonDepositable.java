279870872e7e6c2fa08f3c379ae650f2a86dce2b
a-gohar <90384700+a-gohar@users.noreply.github.com>
2021-12-03 22:08:38 -0500

    Revert "Delete testSimulateNonDepositable.java"
    This reverts commit ebd374a4b52e60f4a55b7fe04c76144e86e56fed.

---
+++ src/test/java/actions/testSimulateNonDepositable.java
@@ -0,0 +1,27 @@
+package actions;
+
+//
+//public class testSimulateNonDepositable {
+//    private NonDepositable assetBond;
+//
+//    @Test
+//    public void testSimulationMature(){
+//        Date endDateMature = new Date(11, 1, 2021);
+//        assetBond = new Bond(2, 7.0, 1000, endDateMature, "Bond1");
+//        SimulateNonDepositableRequest request = new SimulateNonDepositableRequest("jd_123", assetBond);
+//        SimulateNonDepositable simulation = new SimulateNonDepositable(request);
+//        SimulateResponse response = (SimulateResponse) simulation.process();
+//        assertEquals(2000.03, response.getResult(), 0.05);
+//    }
+//
+//    @Test
+//    public void testSimulationImmature(){
+//        Date endDateImmature = new Date(7, 16, 2022);
+//        assetBond = new Bond(2, 7, 1000, endDateImmature, "Bond1");
+//        SimulateNonDepositableRequest request = new SimulateNonDepositableRequest("jd_123", assetBond);
+//        SimulateNonDepositable simulation = new SimulateNonDepositable(request);
+//        SimulateResponse response = (SimulateResponse) simulation.process();
+//        assertEquals(0.0, response.getResult(), 0.05);
+//    }
+//}
+//
