86d9c6f0ec4bebc0e15a5758cd8826c5be289e54
Linh <linhquan.hoang@mail.utoronto.ca>
2021-12-04 19:55:19 -0500

    added helper method to create the string that needs to be made

--- src/main/java/actions/DisplayBudget.java
+++ src/main/java/actions/DisplayBudget.java
@@ -20,6 +20,28 @@ public class DisplayBudget extends Actions {
         this.budget = user.getBudget();
     }
 
+    /**
+     * helper methods
+     */
+    public String createDisplay() {
+        StringBuilder display = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        double goal = 0;
+        double remaining = 0;
+        display.append("Budget Created on: " + budget.getDate());
+        display.append("The total budget amount is: $" + getTotalBudget());
+        display.append("Budget Broken Down:\n");
+
+        for (String category : budget.getCategories()) {
+            goal = budget.getGoalBudget(category);
+            remaining = calculateRemaining(category);
+            temp.append("\t Category Name: " + category + "\n");
+            temp.append("\t\t Goal Budget: $" + goal + "\n");
+            temp.append("\t\t Remaining Budget Unspent: $" + remaining + "\n");
+        }
+        return display.toString();
+    }
+
     public double getTotalBudget() {
         double total = 0;
         for (String category : budget.getCategories()) {
