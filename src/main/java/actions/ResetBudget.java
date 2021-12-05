ace232d24344347d17269a976d0a4ee2814e2435
anita-hp <anita.hessami@mail.utoronto.ca>
2021-12-05 16:46:11 -0500

    Updated ResetBudget and ResetBudgetResponse. Added comment to Budget.

--- src/main/java/actions/ResetBudget.java
+++ src/main/java/actions/ResetBudget.java
@@ -8,37 +8,27 @@ import entity.Owner;
 import entity.OwnerRepository;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class ResetBudget extends Actions{
     private Owner user;
 
     public ResetBudget(ActionRequest request){
         this.user = OwnerRepository.getOwnerRepository().findOwner(request.getUsername());
-
     }
 
     @Override
     public ActionResponse process(){
-//        Budget budget = this.user.getBudget();
-//        HashMap<String, Double> remainingBudget = new HashMap<>(budget.getRemainingBudget());
-//        budget.setRemainingBudget(budget.getGoalBudget());
-//        double totalRemainingBudget = 0;
-//        for(double value : remainingBudget.values()){
-//            totalRemainingBudget +=  value;
-//        }
-//        System.out.println("This is the total remaining budget for last month: " + totalRemainingBudget);
-//        user.addBudgetHistory(totalRemainingBudget);
-//        return new ResetBudgetResponse(true);
         Budget budget = this.user.getBudget();
         ArrayList<String> categories = budget.getCategories();
-
+        if (!(budget.getActive())){
+            return new ResetBudgetResponse(false, 0.0);
+        }
         double totalRemainingBudget = 0;
         for(String category : categories){
-            //budget.setActualBudget(category, 0);
+            budget.setActualBudget(category, 0.0);
             totalRemainingBudget += budget.getGoalBudget(category) - budget.getActualBudget(category);
         }
-        System.out.println("This is the total remaining budget for last month: " + totalRemainingBudget);
-        return new ResetBudgetResponse(true);
+        user.addBudgetHistory(budget);
+        return new ResetBudgetResponse(true, totalRemainingBudget);
     }
 }
