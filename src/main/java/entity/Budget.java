8e6c603ad61e5fd0de8c82befba06c8a8acb3b6d
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2021-12-06 17:04:03 -0500

    Chnaged toString method for Budget

--- src/main/java/entity/Budget.java
+++ src/main/java/entity/Budget.java
@@ -1,10 +1,11 @@
 package entity;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Budget {
-
-
     /**
      * Instances of Budget
      * categories: the types of budget categories (ex: food, clothing, gas...)
@@ -19,20 +20,29 @@ public class Budget {
     private Date date;
     private boolean active;
 
-    public String toString(){
-        if(this.active){
-            return "This is an active budget.";
-        }
-        else{
+    @Override
+    public String toString() {
+        if (this.active) {
+            StringBuilder result = new StringBuilder();
+            result.append("Budget Created on: ").append(this.date);
+            result.append(" Budget Broken Down:\n");
+
+            for (String category : goalBudget.keySet()) {
+                result.append("\t Category Name: ").append(category);
+                result.append("\n\t\t Goal Budget: $").append(goalBudget.get(category));
+                result.append("\n\t\t Money Spent: $").append(actualSpending.get(category));
+            }
+            return result.toString();
+        } else {
             return "This is an inactive budget.";
         }
     }
 
     public Budget(Date date) {
-        this.categories = Arrays.asList( "Groceries", "Gas", "Bill Payments", "Entertainment", "Public Transport");
+        this.categories = Arrays.asList("Groceries", "Gas", "Bill Payments", "Entertainment", "Public Transport");
         this.actualSpending = new HashMap<>();
         this.goalBudget = new HashMap<>();
-        for (String categoryName : this.categories){
+        for (String categoryName : this.categories) {
             this.actualSpending.put(categoryName, 0.0);
             this.goalBudget.put(categoryName, 0.0);
         }
@@ -40,77 +50,96 @@ public class Budget {
         this.active = true;
 
     }
+
     /**
      * Getter for  Date.
-    * getDate returns the date that the Budget was created.*/
+     * getDate returns the date that the Budget was created.
+     */
     public Date getDate() {
         return this.date;
     }
 
     /**
      * setDate replaces the initial date with a new date.
-     * @param newDate: new date of the budget.*/
+     *
+     * @param newDate: new date of the budget.
+     */
     public void setDate(Date newDate) {
         this.date = newDate;
     }
 
     /**
-     * getGoalBudget returns all the goal budgets and current amounts in the budget*/
+     * getGoalBudget returns all the goal budgets and current amounts in the budget
+     */
     public HashMap<String, Double> getGoalBudget() {
         return this.goalBudget;
     }
 
     /**
-     *  setGoalBudget sets the goal budget in the category to its new value.
-     * @param category : the category the user wants budget.
-     * @param newGoalAmount : the new value of the category budget.*/
-    public boolean setGoalBudget(String category, Double newGoalAmount){
-        if (this.goalBudget.containsKey(category)){
+     * setGoalBudget sets the goal budget in the category to its new value.
+     *
+     * @param category      : the category the user wants budget.
+     * @param newGoalAmount : the new value of the category budget.
+     */
+    public boolean setGoalBudget(String category, Double newGoalAmount) {
+        if (this.goalBudget.containsKey(category)) {
             goalBudget.replace(category, newGoalAmount);
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    /** getGaolBudget gets the goal amount spent in the category.
+    /**
+     * getGaolBudget gets the goal amount spent in the category.
+     *
      * @param category : the category chosen to know the goal amount.
-     * */
-    public Double getGoalBudget(String category){
+     */
+    public Double getGoalBudget(String category) {
         return goalBudget.getOrDefault(category, null);
 
     }
 
 
-    /** setActualBudget sets the actual amount spent in the category to its new value.
-     * @param category : the category the user wants budget.
-     * @param newAmount : the new value of the category budget.*/
+    /**
+     * setActualBudget sets the actual amount spent in the category to its new value.
+     *
+     * @param category  : the category the user wants budget.
+     * @param newAmount : the new value of the category budget.
+     */
 
-    public boolean setActualSpending(String category, Double newAmount){
-        if(actualSpending.containsKey(category)){
+    public boolean setActualSpending(String category, Double newAmount) {
+        if (actualSpending.containsKey(category)) {
             this.actualSpending.replace(category, newAmount);
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    /** getActualBudget gets the actual amount spent in the category.
+    /**
+     * getActualBudget gets the actual amount spent in the category.
+     *
      * @param category : the category chosen to know the amount spent.
-     * */
-    public Double getActualSpending(String category){
+     */
+    public Double getActualSpending(String category) {
         return actualSpending.getOrDefault(category, null);
     }
 
 
-    /**getActive returns whether the client set a budget for the month. True for if they did and false otherwise.
-     * */
-    public boolean getActive(){return this.active;}
+    /**
+     * getActive returns whether the client set a budget for the month. True for if they did and false otherwise.
+     */
+    public boolean getActive() {
+        return this.active;
+    }
 
-    /**setActive makes the budget active or inactive depending on the input.*/
-    public void setActive(boolean set){this.active = set;}
+    /**
+     * setActive makes the budget active or inactive depending on the input.
+     */
+    public void setActive(boolean set) {
+        this.active = set;
+    }
 
 
     /**
