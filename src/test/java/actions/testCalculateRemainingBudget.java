d23504c39a3ab0b60221a46e04bb05721cf77daa
Linh <linhquan.hoang@mail.utoronto.ca>
2021-12-06 21:09:24 -0500

    patched testCalculateRemainingBudget

--- src/test/java/actions/testCalculateRemainingBudget.java
+++ src/test/java/actions/testCalculateRemainingBudget.java
@@ -29,9 +29,6 @@ public class testCalculateRemainingBudget {
         OwnerRepository.getOwnerRepository().addOwner(own);
         owner = OwnerRepository.getOwnerRepository().findOwner("villain");
         budget = new Budget(new Date(1, 1,2021));
-        ActionRequest request = new ActionRequest("villain", Commands.CALCULATEREMAININGBUDGET,
-                new ArrayList<>());
-        action = new CalculateRemainingBudget(request);
     }
 
     @AfterEach
@@ -42,6 +39,9 @@ public class testCalculateRemainingBudget {
     @Test
     public void testNoBudget() {
         String expected = "Budget Inactive or Does Not Exist";
+        ActionRequest request = new ActionRequest("villain", Commands.CALCULATEREMAININGBUDGET,
+                new ArrayList<>());
+        action = new CalculateRemainingBudget(request);
         ActionResponse response = action.process();
         assertEquals(((CalculateRemainingBudgetResponse)response).getResult(), expected);
     }
@@ -50,16 +50,23 @@ public class testCalculateRemainingBudget {
         String expected = "Budget Inactive or Does Not Exist";
         budget.setActive(false);
         owner.setBudget(budget);
+        ActionRequest request = new ActionRequest("villain", Commands.CALCULATEREMAININGBUDGET,
+                new ArrayList<>());
+        action = new CalculateRemainingBudget(request);
         ActionResponse response = action.process();
         assertEquals(((CalculateRemainingBudgetResponse)response).getResult(), expected);
     }
 
     //Tests for an active budget
+    @Test
     public void testHasActiveBudget(){
-        String expected = "You have the following amounts left in each category: \n " +
+        String expected = "You have the following amounts left in each category: \n" +
                 "Groceries: 0.0\n" + "Gas: 0.0\n"+ "Bill Payments: 0.0\n"+ "Entertainment: 0.0\n" +
                 "Public Transport: 0.0\n";
         owner.setBudget(budget);
+        ActionRequest request = new ActionRequest("villain", Commands.CALCULATEREMAININGBUDGET,
+                new ArrayList<>());
+        action = new CalculateRemainingBudget(request);
         ActionResponse response = action.process();
         assertEquals(((CalculateRemainingBudgetResponse)response).getResult(), expected);
     }
