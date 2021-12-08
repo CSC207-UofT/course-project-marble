53229c2f14e69938db761b8612622365d0d6c9fb
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2021-12-08 15:25:18 -0500

    -Deleting ServerTest since not useful -Resolved warnings -Fixed JSONTranslator test

--- src/test/java/server/testJSONTranslator.java
+++ src/test/java/server/testJSONTranslator.java
@@ -7,25 +7,19 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class testJSONTranslator {
     private JSONTranslator json;
-    private ArrayList<FinancialAsset> listOfFinancialAssets;
     private Savings savings;
-    private Bond bond;
-    private ArrayList<FinancialAsset> resultsList;
-    private Bond bondCheck;
     private Savings savingsCheck;
 
     @BeforeEach
     void setUp() {
         this.json = new JSONTranslator();
-        this.bond = new Bond(3, 0.75, 25, new Date(10, 10, 2020), "MonkaS");
         this.savings = new Savings(0.02, "Savings");
-        listOfFinancialAssets = new ArrayList<>();
-        listOfFinancialAssets.add(savings);
-        listOfFinancialAssets.add(bond);
+        if(! new File("DataPersistenceTest.json").exists()){
+            new File("DataPersistenceest.json");
+        }
     }
 
     /**
@@ -33,14 +27,12 @@ public class testJSONTranslator {
      * this test both if an object can be stored and retrieved
      * @throws IOException - read/write error
      */
-    @SuppressWarnings(value="unchecked")
     @Test
-    void testDataPersistenceOnAbstractList() throws IOException {
-        json.writeToJSON(listOfFinancialAssets, "DataPersistenceTest.json");
+    void testDataPersistence() throws IOException {
+        json.writeToJSON(savings, "DataPersistenceTest.json");
         if (new File("DataPersistenceTest.json").exists()) {
             try {
-                resultsList = (ArrayList<FinancialAsset>) json.readFromJSON(listOfFinancialAssets.getClass(),
-                        "DataPersistenceTest.json");
+                savingsCheck = (Savings) json.readFromJSON(Savings.class, "DataPersistenceTest.json");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
@@ -48,9 +40,7 @@ public class testJSONTranslator {
         else{
             assert(false);
         }
-        savingsCheck = (Savings) listOfFinancialAssets.get(0);
-        bondCheck = (Bond) listOfFinancialAssets.get(1);
-        if(savingsCheck == savings & bondCheck == bond){
+        if(savingsCheck.toString().equals(savings.toString())){
             assert(true);
         }
         else{
