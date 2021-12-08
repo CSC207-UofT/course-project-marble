79b92a1f1d335148425d4ba18c89a4ebf9a63b87
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2021-12-08 00:18:29 -0500

    Fixing viewBudget related bugs

--- src/main/java/server/ActionFactory.java
+++ src/main/java/server/ActionFactory.java
@@ -44,6 +44,8 @@ public class ActionFactory {
                 return new ResetBudget(request);
             case VIEWBUDGETPERFORMANCE:
                 return new ViewBudgetPerformance(request);
+            case DISPLAYBUDGET:
+                return new DisplayBudget(request);
             case SIMULATE:
                 return new Simulate(request);
         }
