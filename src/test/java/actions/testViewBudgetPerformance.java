80c74ee05967e05914a68b2e7b2761fa73fbbb1d
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2021-12-08 15:25:18 -0500

    -Deleting ServerTest since not useful -Resolved warnings -Fixed JSONTranslator test

--- src/test/java/actions/testViewBudgetPerformance.java
+++ src/test/java/actions/testViewBudgetPerformance.java
@@ -1,19 +1,7 @@
 package actions;
-import action_request_response.ActionRequest;
-import action_request_response.Commands;
-import action_request_response.DisplayWithdrawalRecordResponse;
-import action_request_response.ViewBudgetPerformanceResponse;
 import entity.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class testViewBudgetPerformance {
     @BeforeEach
@@ -24,26 +12,13 @@ public class testViewBudgetPerformance {
         user.setBudget(budget);
         user.addBudgetHistory(user.getBudget());
     }
+
+    //TODO: make test
+
     @AfterEach
     public void teardown() {
         OwnerRepository.getOwnerRepository().deleteOwner("jd_123");
     }
-
-
-//    @Test
-//    public void testViewBudgetPerformanceSuccess(){
-//        ActionRequest request = new ActionRequest("jd_123", Commands.VIEWBUDGETPERFORMANCE, new ArrayList<>());
-//        ViewBudgetPerformance budgetPerformance = new ViewBudgetPerformance(request);
-//        ViewBudgetPerformanceResponse response = (ViewBudgetPerformanceResponse) budgetPerformance.process();
-//        HashMap<Date, HashMap<String,Double>> expected = new HashMap<>();
-//        List<String> categories = Arrays.asList( "Groceries", "Furniture", "Restaurant", "Vacation", "Gas", "Recurring Bill Payment", "Health and Beauty", "Home Improvement", "Entertainment", "Public Transportation and Parking");
-//        expected.put(new Date(1,1,2021),new HashMap<>());
-//        for (String category: categories){
-//            expected.get( new Date(1, 1, 2021)).put(category,0.00);
-//        }
-//
-//        assertEquals(expected.toString(), response.getDisplay());
-//    }
 
 
 
