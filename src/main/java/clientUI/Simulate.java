d94520b5b3c981832974cec542b0d006813f7955
ruiting-chen <51133017+ruiting-chen@users.noreply.github.com>
2021-12-08 00:39:40 -0500

    Fixed bugs related to Simulate and ViewBudgetPerformance

--- src/main/java/clientUI/Simulate.java
+++ src/main/java/clientUI/Simulate.java
@@ -8,6 +8,7 @@ import action_request_response.SimulateResponse;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Simulate extends SendReceive{
@@ -16,9 +17,22 @@ public class Simulate extends SendReceive{
     }
     public void simulate(String username){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a date to simulate the worth of asset.");
-        String endDate = sc.nextLine();
-        ActionRequest request = new ActionRequest(username, Commands.SIMULATE, new ArrayList<>());
+        System.out.println("Please enter the year to simulate the worth of asset");
+        int year = Integer.parseInt(sc.nextLine());
+        System.out.println("Please enter the month to simulate the worth of asset");
+        int month = Integer.parseInt(sc.nextLine());
+        System.out.println("Please enter the day to simulate the worth of asset");
+        int day = Integer.parseInt(sc.nextLine());
+
+        String str_month = "0" + month;
+        str_month = str_month.substring(str_month.length() - 2);
+
+        String str_day = "0" + day;
+        str_day = str_day.substring(str_day.length() - 2);
+
+        String date = year + "-" + str_month + "-" + str_day;
+
+        ActionRequest request = new ActionRequest(username, Commands.SIMULATE, new ArrayList<>(List.of(date)));
         sendReceiveObject(request);
         if (response != null) {
             System.out.println(((SimulateResponse) response).getResult());
